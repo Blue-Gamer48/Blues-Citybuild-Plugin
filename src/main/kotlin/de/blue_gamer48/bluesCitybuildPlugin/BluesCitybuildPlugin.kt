96b42f4ec37520a75aa360fdeb006e96f0da59ce
Blue-Gamer48 <leonfritzsch11@gmail.com>
2025-09-24 17:19:27 +0200

    Initial commit

---
+++ src/main/kotlin/de/blue_gamer48/bluesCitybuildPlugin/BluesCitybuildPlugin.kt
@@ -0,0 +1,26 @@
+package de.blue_gamer48.bluesCitybuildPlugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BluesCitybuildPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+        logger.info("""
+        ####################################
+        Plugin Blues Citybuild Core Starting
+        Developer: Blue_Gamer48
+        ####################################
+        """.trimIndent())
+
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+        logger.info("""
+        ####################################
+        Plugin Blues Citybuild Plugin Stopping
+        ####################################
+        """.trimIndent())
+    }
+}
---
+++ src/main/kotlin/de/blue_gamer48/bluesCitybuildPlugin/commands/Rand_and_Wand_Commands.kt
@@ -0,0 +1,6 @@
+package de.blue_gamer48.bluesCitybuildPlugin.commands
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Rand_and_Wand_Commands {
+}
